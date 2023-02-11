9ee5e8b5672a2dd857d28388fa44cb8f535cfbd9
Elsa Firmin <elsafirmin@hotmail.fr>
2023-02-11 16:30:06 +0100

    Add switch and function omp

--- OmpTrace.cpp
+++ OmpTrace.cpp
@@ -19,13 +19,54 @@ int main(int argc, char* argv[])
     if (argc < 3)
         goto finally;
     img.SetSize(LARGEUR, HAUTEUR);
+    omp_set_num_threads(argv[2]);
     for (int m = 0; m < LARGEUR; m++)  // Thread1 Thread2 etc.
     {
+        #pragma omp parallel for
         for (int n = 0; n < HAUTEUR; n++)
         {
-            pixel.Blue = 0;
-            pixel.Green = 0 ;
-            pixel.Red = 255;
+            switch(omp_get_num_threads()){
+                case 0:
+                    pixel.Blue = 0;
+                    pixel.Green = 0;
+                    pixel.Red = 255;
+                    break;
+                case 1:
+                    pixel.Blue = 0;
+                    pixel.Green = 255;
+                    pixel.Red = 0;
+                    break;
+                case 2:
+                    pixel.Blue = 255;
+                    pixel.Green = 0;
+                    pixel.Red = 0;
+                    break;  
+                case 3:
+                    pixel.Blue = 0;
+                    pixel.Green = 255;
+                    pixel.Red = 255;
+                    break;  
+                case 4:
+                    pixel.Blue = 255;
+                    pixel.Green = 0;
+                    pixel.Red = 255;
+                    break; 
+                case 5:
+                    pixel.Blue = 255;
+                    pixel.Green = 255;
+                    pixel.Red = 0;
+                    break;       
+                case 6:
+                    pixel.Blue = 255;
+                    pixel.Green = 255;
+                    pixel.Red = 255;
+                    break;  
+                case 7:
+                    pixel.Blue = 0;
+                    pixel.Green = 0;
+                    pixel.Red = 0;
+                    break;                   
+            }
 
             img.SetPixel(m, n, pixel);
         } // columns
